fb8b5e8b08f8e7d4b5bc51022f539670f9a0a2b6
Sean Anderson <sean.anderson781@gmail.com>
2018-05-10 15:36:34 +0100

    Added Player.prototype.render function - GAME LOADS ON PAGE

--- js/app.js
+++ js/app.js
@@ -40,6 +40,10 @@ var Player = function(x, y, speed){
 let allEnemies = [];
 let player = new Player(200,400);
 
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
