02608724c280e59847be4175759398177b6b069c
Sean Anderson <sean.anderson781@gmail.com>
2018-05-15 10:00:20 +0100

    Added lives function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 // ***** CLASSIC ARCADE GAME CLONE *****
 
+let score = 0;
+let lives = 3;
+
 /*
  *** AVIOD ENEMIES ***
  */
@@ -9,7 +12,7 @@ let enemyPosition = [61, 144, 227]; // Position "y" where the enemies will are c
 let allEnemies = [];
 
 // Enemies our player must avoid
-let Enemy = function(x, y, speed) {
+let Enemy = function (x, y, speed) {
     // Variables applied to each of the instances
     this.x = x; // x and y coordinates
     this.y = y;
@@ -20,14 +23,14 @@ let Enemy = function(x, y, speed) {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // Multiplies the speed by the dt parameter on the x axis
     this.x += this.speed * dt;
 
     // Reposition the enemy after they leave the page 
     if (this.x >= 505) { // Canvas is 505 wide
         this.x = -150;   // Enemy are 100 wide
-        //Speed of enemy
+        // Speed of the emeny
         this.speed = 100 + Math.floor(Math.random() * 550);
     }
 
@@ -36,22 +39,34 @@ Enemy.prototype.update = function(dt) {
         player.x + 80 > this.x &&
         player.y < this.y + 60 &&
         60 + player.y > this.y) {
-            // Collision detected, reset player
-            player.resetPlayer();
-            //Decrease score
-            score -= 15;
-            document.querySelector('.score').innerHTML = score;
+        // Collision detected, reset player
+        player.resetPlayer();
+        //Decrease score
+        score -= 15;
+        document.querySelector('.score').innerHTML = score;
+        // Remove life
+        if (lives === 3) {
+            document.getElementsByClassName('mini-player')[2].remove();
+            lives -= 1;
+        } else if (lives === 2) {
+            document.getElementsByClassName('mini-player')[1].remove();
+            lives -= 1;
+        } else if (lives === 1) {
+            document.getElementsByClassName('mini-player')[0].remove();
+            alert('Game Over!!!\nYou Scored ' + score + ' Points');
+            location.reload();
+        }
     }
 };
 
 // Function to randomly place the emeny
-enemyPosition.forEach(function(posY) {
+enemyPosition.forEach(function (posY) {
     enemy = new Enemy(-150, posY, 100 + Math.floor(Math.random() * 550));
     allEnemies.push(enemy);
 });
 
 // Draw the enemy on the screen
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -61,31 +76,29 @@ Enemy.prototype.render = function() {
  */
 
 // Player variables
-let score = 0;
-let Player = function(x, y,) {  // player moves along x, y axis
+let Player = function (x, y, ) {  // player moves along x, y axis
     // Variables applied to each of the instances
     this.x = x; // x and y coordinates
     this.y = y;
     // The image/sprite of the player
     this.sprite = 'images/char-boy.png';
 };
-let player = new Player(200,400); // Starting location of the player
-
+let player = new Player(200, 400); // Starting location of the player
 
 // Draw the player on the screen
-Player.prototype.render = function(){
+Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Reset player to starting position
-Player.prototype.resetPlayer = function() {
+Player.prototype.resetPlayer = function () {
     this.x = 200;
     this.y = 400;
 }
 
 // Called everytime the players position changes
-player.update = function(dt) {
-    // Player position will be reset after reaching the top water area
+player.update = function (dt) {
+    // Reset player position after reaching the top water area
     if (this.y < 0) {
         setTimeout(() => {
             this.resetPlayer();
@@ -99,9 +112,9 @@ player.update = function(dt) {
 
 /*
  *** Keypress lister - Supplied by Udacity ***
- */ 
+ */
 // Keypresses are sent to the Player.handleInput() method.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -112,18 +125,18 @@ document.addEventListener('keyup', function(e) {
 });
 
 // Keypress function to direct player
-player.handleInput = function(keypress){
+player.handleInput = function (keypress) {
 
-    if(keypress === 'left' && this.x >= 100){
+    if (keypress === 'left' && this.x >= 100) {
         this.x -= 100;
     }
-    if(keypress === 'up' && this.y >= 30){
+    if (keypress === 'up' && this.y >= 30) {
         this.y -= 82.5;
     }
-    if(keypress === 'right' && this.x <= 305){
+    if (keypress === 'right' && this.x <= 305) {
         this.x += 100;
     }
-    if(keypress === 'down' && this.y <= 350){
+    if (keypress === 'down' && this.y <= 350) {
         this.y += 82.5;
     }
 };
